38118459357e946fa214606448fddd8eb1b7f95e
fuuuuumin <tmfha3@gmail.com>
2023-10-08 18:02:39 +0900

    カテゴリの選択・表示ができる

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,13 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
   validates :name, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
 
   scope :latest, -> { order(created_at: :desc)}
   scope :old, -> { order(created_at: :asc)}
   scope :deadline, -> { order(deadline: :asc)}
+
 
 end
